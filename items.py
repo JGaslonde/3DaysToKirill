efb3c8f9faadd572e6b1e5b8d7d297bcd2be9073
Daniel Hendry <djhendry11@hotmail.com>
2015-10-19 13:02:45 +0100

    Initial versions of the files.

---
+++ items.py
@@ -0,0 +1,72 @@
+item_key_1 = {
+	"id": "key 1",
+
+	"name": "Library Card",
+
+	"description":
+	"""This is a thing."""
+}
+
+item_key_2 = {
+	"id": "key 2",
+
+	"name": "Library Card",
+
+	"description":
+	"""This is a thing."""
+}
+
+item_weapon = {
+	"id": "key 3",
+
+	"name": "a weapon",
+
+	"description":
+	"""This is a thing."""
+}
+
+food_burger = {
+	"id": "burger",
+
+	"name": "a burger",
+
+	"description":
+	"""A tasty burger from everyone's favourite burger chain....Wendy's.""",
+
+	"health": 10
+}
+
+food_salad = {
+	"id": "salad",
+
+	"name": "a salad",
+
+	"description":
+	"""A healthy salad grown fresh in an alley near Tiger Tiger.....huh....
+	nothing but leaves.""",
+
+	"health": 15
+}
+
+food_kebab = {
+	"id": "kebab",
+
+	"name": "a kebab",
+
+	"description":
+	"""One of the greasiest kebabs you have ever seen. You wonder how it made it
+	here on its own.""",
+
+	"health": 5
+}
+
+food_banana= {
+	"id": "",
+
+	"name": "a banana",
+
+	"description":
+	"""hahah bottom """,
+
+	"health": 13
+}
---
+++ player.py
@@ -0,0 +1,12 @@
+player = {
+    "name": "Keith",
+
+    "health": 1000, 
+
+    "alive": True,
+    
+    "inventory": [],
+
+    "score" = 0
+
+}
