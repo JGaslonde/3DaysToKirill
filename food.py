89bbbc19485ec8f609f008e9412a69acd9eae414
Daniel Hendry <djhendry11@hotmail.com>
2015-10-20 11:52:59 +0100

    Created food file to include all food items only

---
+++ food.py
@@ -0,0 +1,39 @@
+food = [food_burger, food_salad, food_kebab, food_banana]
+
+food_burger = {
+	"id": "burger",
+
+	"name": "a burger",
+
+	"description":
+	"""A tasty burger from everyone's favourite burger chain....Wendy's.""",
+}
+
+food_salad = {
+	"id": "salad",
+
+	"name": "a salad",
+
+	"description":
+	"""A healthy salad grown fresh in an alley near Tiger Tiger.....huh....
+	nothing but leaves.""",
+}
+
+food_kebab = {
+	"id": "kebab",
+
+	"name": "a kebab",
+
+	"description":
+	"""One of the greasiest kebabs you have ever seen. You wonder how it made it
+	here on its own.""",
+}
+
+food_banana= {
+	"id": "",
+
+	"name": "a banana",
+
+	"description":
+	"""hahah bottom """,
+}
